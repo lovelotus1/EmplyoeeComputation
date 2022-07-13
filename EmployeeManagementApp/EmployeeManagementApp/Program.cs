048345008c41b37e9516f323db6ad63b09af52b7
lovelotus <opera.prem@gmail.com>
2022-07-13 13:18:09 +0530

    Using Switch Case for EmpWage

--- EmployeeManagementApp/EmployeeManagementApp/Program.cs
+++ EmployeeManagementApp/EmployeeManagementApp/Program.cs
@@ -1,19 +1,22 @@
-﻿int EMP_RATE_PER_HOUR = 20;
-int IS_PART_TIME = 1;
-int IS_FULL_TIME = 2;
+﻿const int IS_PART_TIME = 1;
+const int IS_FULL_TIME = 2;
+const int EMP_RATE_PER_HOUR = 20;
+
 int empHrs = 0;
 int empWage = 0;
 Random random = new Random();
 int empCheck = random.Next(0, 3);
 
-if (empCheck == IS_PART_TIME)
+switch (empCheck)
 {
-    empHrs = 4;
-}
-else if (empCheck == IS_FULL_TIME)
-{
-    empHrs = 8;
-}
+    case IS_PART_TIME:
+        empHrs = 4;
+        break;
+    case IS_FULL_TIME:
+        break;
 
+
+}
 empWage = empHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("Emp Wage : " + empWage);
+
