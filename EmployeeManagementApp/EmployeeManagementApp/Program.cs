ec177498bef8c4c6c8f28a173a2459e864c31e7f
lovelotus <opera.prem@gmail.com>
2022-07-13 12:24:05 +0530

    Added Employee Attendance

--- EmployeeManagementApp/EmployeeManagementApp/Program.cs
+++ EmployeeManagementApp/EmployeeManagementApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Employee Management Program");
+﻿
+int IS_FULL_TIME = 1;
+Random random = new Random();
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
+{
+    Console.WriteLine("Employee is Present");
+
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
+
