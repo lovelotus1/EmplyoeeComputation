8b81262a1b5bde65bef776f6eace61162abd744c
lovelotus <opera.prem@gmail.com>
2022-07-13 20:44:46 +0530

    Added Working Days Wage

--- EmployeeManagementApp/EmployeeManagementApp/Program.cs
+++ EmployeeManagementApp/EmployeeManagementApp/Program.cs
@@ -1,9 +1,10 @@
 ﻿const int IS_PART_TIME = 1;
 const int IS_FULL_TIME = 2;
 const int EMP_RATE_PER_HOUR = 20;
+const int NUM_OF_WORKING_DAYS = 2;
+int empHrs = 0, empWage = 0, totalEmpWage = 0;
+for(int day = 0; day < NUM_OF_WORKING_DAYS;day++){
 
-int empHrs = 0;
-int empWage = 0;
 Random random = new Random();
 int empCheck = random.Next(0, 3);
 
@@ -13,10 +14,17 @@ switch (empCheck)
         empHrs = 4;
         break;
     case IS_FULL_TIME:
+            empHrs = 8;
         break;
-
+    default:
+        empHrs = 0;
+        break;
 
 }
 empWage = empHrs * EMP_RATE_PER_HOUR;
-Console.WriteLine("Emp Wage : " + empWage);
+   totalEmpWage += empWage;
+    Console.WriteLine("Emp Wage : " + empWage);
+}
+Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+
 
